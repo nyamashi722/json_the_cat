df3d96d5668d88962ed3b5321aa2cbe09b2d6d61
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-28 16:31:37 -0400

    created a node app that reads info about cats and returns the description to the terminal, edge cases considered

---
+++ breedFetcher.js
@@ -0,0 +1,22 @@
+const request = require("request")
+const userInput = process.argv[2];
+
+
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput}`, (error, response, body) => {
+
+  if (error) {
+    console.log("Page cannot be found")
+  } else {
+    const data = JSON.parse(body)
+    if (data.length > 0) {
+      console.log(data[0].description)
+    } else {
+      console.log("Breed not found")
+    };
+  }
+  })
+
+
+
+
