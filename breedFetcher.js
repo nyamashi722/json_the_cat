3ffc05ece4776757d73a455112d7d18ff3d15098
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-28 16:34:59 -0400

    linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,20 @@
-const request = require("request")
+const request = require("request");
 const userInput = process.argv[2];
 
 
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput}`, (error, response, body) => {
-
   if (error) {
-    console.log("Page cannot be found")
+    console.log("Page cannot be found");
   } else {
-    const data = JSON.parse(body)
+    const data = JSON.parse(body);
     if (data.length > 0) {
-      console.log(data[0].description)
+      console.log(data[0].description);
     } else {
-      console.log("Breed not found")
-    };
+      console.log("Breed not found");
+    }
   }
-  })
+});
 
 
 
